f1813c7c5bc2eeddb26e9f16f33351d840736b03
UDAYhere <uday92689@gmail.com>
2025-06-20 11:21:45 +0530

    working with API

--- API/app.js
+++ API/app.js
@@ -3,8 +3,27 @@
 
 let url="https://catfact.ninja/fact";
 
-fetch(url).then((res)=>{
-    console.log(res)
-}).catch((err)=>{
-    console.log(err);
-})
+// fetch(url).then((res)=>{
+//     console.log(res)
+//     res.json().then((data)=>{console.log(data)})
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+//now i ma going to use a async function to fetch the api and also going to use await because js is asynchrnous in nature
+
+async function fetchData() {
+    try{
+        let res=await fetch(url);
+        let data=await res.json();
+        console.log(data.fact);
+
+        let res2=await fetch(url);
+        let data2=await res2.json();
+        console.log(data2.fact);
+    }catch(err){
+        console.log("Error fetching data:", err);
+    }
+}
+
+fetchData();
